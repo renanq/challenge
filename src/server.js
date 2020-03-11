b26d1cd33911f26488ac050b98cfb944d9bfc4cb
Renan Queiroz <renanq@gmail.com>
2020-03-11 18:12:10 -0300

    Ambiente preparado para o desenvolvimento

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const { GraphQLServer } = require("graphql-yoga");
+const path = require("path");
+const resolvers = require("./resolvers");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://graphql:101182@cluster0-7spmy.mongodb.net/test?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, () => console.log("banco conectado!"));
+
+const server = new GraphQLServer({
+  typeDefs: path.resolve(__dirname, "schema.graphql"),
+  resolvers
+});
+
+server.start(4000, () => console.log("Servidor rodando na porta 4000"));
